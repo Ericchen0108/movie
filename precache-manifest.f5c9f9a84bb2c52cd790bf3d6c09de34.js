2f0ba9e7e88390987b1ac1c37aca9029d66f798d
Eric Chen <eric19920108@gmail.com>
2020-10-16 02:59:44 +0800

    Updates

---
+++ precache-manifest.f5c9f9a84bb2c52cd790bf3d6c09de34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d38799965955be51c98a6bd3ce14fc2",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "4add7a64cc0357a04c26",
+    "url": "/movie/static/css/main.71991861.chunk.css"
+  },
+  {
+    "revision": "0a3647222620c5486346",
+    "url": "/movie/static/js/2.9323defc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie/static/js/2.9323defc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4add7a64cc0357a04c26",
+    "url": "/movie/static/js/main.065f2185.chunk.js"
+  },
+  {
+    "revision": "ed84cc0093c15cee011f",
+    "url": "/movie/static/js/runtime-main.2bc06d1f.js"
+  }
+]);
